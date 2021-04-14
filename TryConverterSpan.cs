57338422b9da49d70e05cf8ce99563e39a6d3c56
Steven Retz <sretz@innovoft.com>
2021-04-14 20:03:12 -0300

    added delegate public delegate bool TryConverterSpan<TInput, TOutput>(Span<TInput> intput, out TOutput output);

---
+++ TryConverterSpan.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft
+{
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+	public delegate bool TryConverterSpan<TInput, TOutput>(Span<TInput> intput, out TOutput output);
+#endif //NETSTANDARD2_1 || NET5_0_OR_GREATER
+}
