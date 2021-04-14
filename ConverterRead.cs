00e19616893cdc0febe2a155fc84702227575a63
Steven Retz <sretz@innovoft.com>
2021-04-14 19:56:59 -0300

    added delegate public delegate TOutput ConverterRead<TInput, out TOutput>(ReadOnlySpan<TInput> input);

---
+++ ConverterRead.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft
+{
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+	public delegate TOutput ConverterRead<TInput, out TOutput>(ReadOnlySpan<TInput> input);
+#endif
+}
