26b6b49ed565fb141ab1370d93b67fcf9b7101c4
Steven Retz <sretz@innovoft.com>
2021-04-14 19:55:12 -0300

    added delegate public delegate TOutput ConverterSpan<TInput, out TOutput>(Span<TInput> input);

---
+++ ConverterSpan.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft
+{
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+	public delegate TOutput ConverterSpan<TInput, out TOutput>(Span<TInput> input);
+#endif
+}
