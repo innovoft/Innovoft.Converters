326de49f3a01876a1210355db138b03f23b63f61
Steven Retz <sretz@innovoft.com>
2021-04-14 20:13:00 -0300

    added class public static class CharConverter

---
+++ CharConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft
+{
+	public static class CharConverter
+	{
+		#region Constants
+		public const char NUL = '\0';
+		public const char CR = '\r';
+		public const char LF = '\n';
+		public const char Tab = '\t';
+		public const char Space = ' ';
+		public const char QuoteDouble = '\"';
+		public const char ParenthesisOpen = '(';
+		public const char ParenthesisClose = ')';
+		public const char CurveOpen = ParenthesisOpen;
+		public const char CurveClose = ParenthesisClose;
+		public const char BracketOpen = '[';
+		public const char BracketClose = ']';
+		public const char SquareOpen = BracketOpen;
+		public const char SquareClose = BracketClose;
+		public const char BraceOpen = '{';
+		public const char BraceClose = '}';
+		public const char Comma = ',';
+		public const char Z = 'Z';
+		public const char z = 'z';
+		#endregion //Constants
+	}
+}
