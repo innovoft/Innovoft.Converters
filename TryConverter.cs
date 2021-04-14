7dd38cd9d8af2598da7ba927cf4037f9281f4bf2
Steven Retz <sretz@innovoft.com>
2021-04-14 20:00:55 -0300

    added delegate public delegate bool TryConverter<in TInput, TOutput>(TInput intput, out TOutput output);

---
+++ TryConverter.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft
+{
+	public delegate bool TryConverter<in TInput, TOutput>(TInput intput, out TOutput output);
+}
