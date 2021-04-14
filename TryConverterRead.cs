e58c7bbaf68deb404e62813b4cc9dc576a69f679
Steven Retz <sretz@innovoft.com>
2021-04-14 20:04:44 -0300

    added delegate public delegate bool TryConverterRead<TInput, TOutput>(ReadOnlySpan<TInput> intput, out TOutput output);

---
+++ TryConverterRead.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innovoft
+{
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+	public delegate bool TryConverterRead<TInput, TOutput>(ReadOnlySpan<TInput> intput, out TOutput output);
+#endif //NETSTANDARD2_1 || NET5_0_OR_GREATER
+}
