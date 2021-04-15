c289ab8b448339a86ef97732a7154bb5222075d2
Steven Retz <sretz@innovoft.com>
2021-04-15 13:29:52 -0300

    added consts Digit0???

--- DateTimeConverters.cs
+++ DateTimeConverters.cs
@@ -8,7 +8,9 @@ namespace Innovoft
 	{
 		#region Constants
 		private const char ZChar = CharConverters.Z;
+		private const char Digit0Char = CharConverters.Digit0;
 		private const byte ZByte = ASCIIConverters.Z;
+		private const byte Digit0Byte = ASCIIConverters.Digit0;
 		#endregion //Constants
 	}
 }
