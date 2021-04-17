73b1cd61e1435b3deed50dbbeb8168ed9745e167
Steven Retz <sretz@innovoft.com>
2021-04-17 11:12:20 -0300

    renamed delegate to TryConverterReadOnlySpan

--- TryConverterReadOnlySpan.cs
+++ TryConverterReadOnlySpan.cs
@@ -5,6 +5,6 @@ using System.Text;
 namespace Innovoft
 {
 #if NETSTANDARD2_1 || NET5_0_OR_GREATER
-	public delegate bool TryConverterRead<TInput, TOutput>(ReadOnlySpan<TInput> intput, out TOutput output);
+	public delegate bool TryConverterReadOnlySpan<TInput, TOutput>(ReadOnlySpan<TInput> intput, out TOutput output);
 #endif //NETSTANDARD2_1 || NET5_0_OR_GREATER
 }
